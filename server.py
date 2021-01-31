1823334636bbe5b6199c7f81d6258d3b8bdf2014
Timothy Qin <timothy.t.qin@gmail.com>
2021-01-31 03:38:54 -0600

    swipe direction capture working

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
+from flask import Flask
+from flask_socketio import SocketIO, send
+import cv2
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2
 from statistics import mean 
 
-# swiper start swiping
 
 def leftOut(x):
     return x < 580
@@ -20,12 +22,52 @@ def bottomOut(y):
 def inBounds(x, y):
     return not (leftOut(x) or rightOut(x) or topOut(y) or bottomOut(y))
 
+#Reads in the 33k lines of XML
+haar_cascade = cv2.CascadeClassifier("haar_face.xml")
+
 cap = cv2.VideoCapture(0)
 
 in_frame = False
 draw = True
 
-while(True):
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "mysecret"
+
+socketIo = SocketIO(app, cors_allowed_origins="*")
+
+app.debug = True
+
+app.host = "localhost"
+
+@socketIo.on('connect')
+def on_connect():
+  global in_frame
+  video_capture = cv2.VideoCapture(0)
+  print('connected')
+  counter = 0
+  while True:
+      # Capture frame-by-frame
+      ret, frame = video_capture.read()
+
+      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+      faces = haar_cascade.detectMultiScale(
+          gray,
+          scaleFactor=1.1,
+          minNeighbors=3,
+      )
+
+      if len(faces) > 0:
+          counter += 1
+          if counter > 30:
+              print('found face')
+              socketIo.emit('face')
+              video_capture.release()
+              cv2.destroyAllWindows()
+              break
+      if len(faces) == 0:
+          counter = 0
+  while(True):
     ret, frame = cap.read()
 
     Z = frame.reshape((-1,3))
@@ -87,3 +129,15 @@ while(True):
 
 cap.release()
 cv2.destroyAllWindows()
+
+@socketIo.on("message")
+def handleMessage(msg):
+    print(msg)
+    send(msg, broadcast=True)
+    return None
+
+if __name__ == "__main__":
+    socketIo.run(app)
+
+
+
